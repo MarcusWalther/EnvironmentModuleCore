32828432b7288641928fea15d8aebe258ae091f7
Marcus Walther <walther72@hotmail.de>
2018-06-07 16:24:33 +0200

    - Added StartSampleEnvironment Script

---
+++ Samples/StartSampleEnvironment.ps1
@@ -0,0 +1,4 @@
+$global:VerbosePreference = "Continue"
+$env:PSModulePath = "$PSScriptRoot;$(Resolve-Path (Join-Path $PSScriptRoot '..\Tmp\Modules'));$(Resolve-Path (Join-Path $PSScriptRoot '..\Test'))"
+#$env:PSModulePath = "$env:PSModulePath;$(Resolve-Path (Join-Path $PSScriptRoot '..\..\'))"
+Update-EnvironmentModuleCache
